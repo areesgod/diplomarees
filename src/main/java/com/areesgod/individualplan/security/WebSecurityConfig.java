1465cfbbea2db6be0657b6f8ff3f09211a01845a
areesgod <73094827+areesgod@users.noreply.github.com>
2023-06-05 08:50:02 +0600

    LogIn fixed

--- src/main/java/com/areesgod/individualplan/security/WebSecurityConfig.java
+++ src/main/java/com/areesgod/individualplan/security/WebSecurityConfig.java
@@ -85,6 +85,7 @@ public class WebSecurityConfig { // extends WebSecurityConfigurerAdapter {
         .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
         .authorizeRequests().antMatchers("/api/auth/**").permitAll()
         .antMatchers("/api/test/**").permitAll()
+        .antMatchers("/behavior/getAll","/education/getAll","/edumeth/getAll","/publicwork/getAll","/science/getAll").permitAll()
         .anyRequest().authenticated();
     
     http.authenticationProvider(authenticationProvider());
